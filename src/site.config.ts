3611e205c75f7767369940293cda648b29a55e21
Denis Karpovskii <geext29@tuta.io>
2026-05-23 17:58:28 +0400

    Add social links

---
+++ src/site.config.ts
@@ -0,0 +1,23 @@
+export const siteConfig = {
+	name: "Denis Karpovskii",
+	profile: {
+		about: "About placeholder. A short personal introduction will live here soon.",
+		socialLinks: [
+			{
+				label: "Instagram",
+				icon: "instagram",
+				href: "https://www.instagram.com/geext29/",
+			},
+			{
+				label: "LinkedIn",
+				icon: "linkedin",
+				href: "https://www.linkedin.com/in/geext/",
+			},
+			{
+				label: "GitHub",
+				icon: "github",
+				href: "https://github.com/geext-dk",
+			},
+		],
+	},
+} as const;
