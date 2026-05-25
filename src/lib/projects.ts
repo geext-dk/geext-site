2c2c3c560a8bcf5fc1da10c8394bcec483e400d4
Denis Karpovskii <geext29@tuta.io>
2026-05-26 00:58:42 +0400

    Adjustments

--- src/lib/projects.ts
+++ src/lib/projects.ts
@@ -4,6 +4,7 @@ export type Project = {
   title: string;
   description: string;
   image: string;
+  sourceUrl?: string;
   url?: string;
 };
 
--- src/site.config.ts
+++ src/site.config.ts
@@ -1,7 +1,7 @@
 export const siteConfig = {
 	name: "Denis Karpovskii",
 	profile: {
-		about: "About placeholder. A short personal introduction will live here soon.",
+		about: "A proud software engineer from Russia, living in Georgia 🇬🇪. Also a film photographer, a gamer, a music lover and a sweet tooth",
 		avatarUrl: "https://photos.geext.xyz/images/109cb2c22dcdbe34ab6d02725798a8fa6462a120dee2efb3c9731b4eea7b19cd.avif",
 		backgroundPhotoId: "3c26b87cf16e0cf227b2ec922577406d3056d1f30fbe3a5de3843cbf2529e4ab",
 		socialLinks: [
